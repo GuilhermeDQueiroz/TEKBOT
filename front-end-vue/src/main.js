7ccc90d79614e0d986295392137326ff9828e736
GessicaNog <gessicanogueira928@gmail.com>
2025-10-05 18:21:52 -0300

    adicionando o pinia

--- front-end-vue/src/main.js
+++ front-end-vue/src/main.js
@@ -8,14 +8,17 @@ import { library } from '@fortawesome/fontawesome-svg-core'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 
 import { faSignOutAlt } from '@fortawesome/free-solid-svg-icons'
+import { createPinia } from 'pinia'
 
 library.add(faSignOutAlt)
 
-
+const pinia = createPinia()
 const app = createApp(App)
 
 app.component('font-awesome-icon', FontAwesomeIcon)
 
 app.use(router)
+app.use(pinia)
+
 
 app.mount('#app')
